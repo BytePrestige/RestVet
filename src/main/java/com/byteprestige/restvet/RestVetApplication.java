6511a3cf2ed7c70ac61f0db8c29c3b01a217e36d
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-01-12 18:12:54 +0000

    chore: Update package

--- src/main/java/com/byteprestige/restvet/RestVetApplication.java
+++ src/main/java/com/byteprestige/restvet/RestVetApplication.java
@@ -1,4 +1,4 @@
-package com.restvet.RestVet;
+package com.byteprestige.restvet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
