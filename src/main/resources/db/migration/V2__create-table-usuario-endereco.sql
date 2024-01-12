6654e9c5cc36a87badc4d5f50828d83b3aee9d4b
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-01-12 16:14:32 -0300

    chore: Update project
    wrong database information

--- src/main/resources/db/migration/V2__create-table-usuario-endereco.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuario_endereco
-(
-    id_usuario  INT,
-    id_endereco INT,
-    PRIMARY KEY (id_usuario, id_endereco),
-    FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario),
-    FOREIGN KEY (id_endereco) REFERENCES endereco (id_endereco)
-);
